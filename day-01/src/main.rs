8e6608af500e8de5efb614eccc0b29b57e188c8c
Kieran Warwick <kwarwick97@gmail.com>
2022-12-06 21:18:56 +0000

    day 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "input.txt";
+
+    println!("In file {}", file_path);
+
+    let file = File::open(file_path).expect("Should have been able to read the file");
+    let reader = BufReader::new(file);
+
+    let mut i = 0;
+    let mut acc = 0;
+    let mut vec = Vec::new();
+    for line in reader.lines() {
+        let line = line.expect("msg");
+        if line.is_empty() {
+            vec.push(acc);
+            i = i + 1;
+            acc = 0;
+            continue;
+        }
+        acc = acc + line.parse::<i32>().expect("should be a number");
+    }
+    vec.sort();
+    match vec[..] {
+        [.., third, second, first] => {
+            println!("Most: {}", first);
+            println!("Top three total: {}", first + second + third);
+        },
+        _  => print!("Not enough elves :(")
+    }
 }
